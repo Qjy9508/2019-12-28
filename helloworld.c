ea2c7cc3ee484d2b58b9e8be69ff6ce89ecc6023
Qjy9508 <827945169@qq.com>
2019-12-28 13:22:46 +0800

    为helloworld函数添加了test函数作为测试使用

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int test(int i);
+
 int main()
 {
 	printf("hello world!\n");
+	int i = 3;
+	test(i;
 	return 0;
+}
+
+int test(int i)
+{
+	printf("这个函数是为了测试远程提交新增加的函数\n");
 }
